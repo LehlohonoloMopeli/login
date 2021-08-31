c3da5920bae894d3aee5a47e7aa96afe5497f930
Lehlohonolo Mopeli <71072285+LehlohonoloMopeli@users.noreply.github.com>
2021-08-31 11:56:42 +0200

    Added requirements

--- app/registration_service/service.py
+++ app/registration_service/service.py
@@ -2,6 +2,8 @@ from app.database import Session
 from .model import RegistrationModel
 from app.database.user import User
 
+from pyisemail import is_email
+from fastapi.encoders import jsonable_encoder
 
 class Registration:
 
@@ -20,6 +22,28 @@ class Registration:
             password = self.__inputs.password
         )
 
+        #Validators
+
+        if is_email(self.__inputs.email, check_dns=True) == False:
+            return {
+            "status": "failed",
+            "message": "Email format is incorrect!"
+        }
+
+        if self.__inputs.password != self.__inputs.confirm_password:
+            return {
+            "status": "failed",
+            "message": "Passswords do not match!"
+        }
+
+        query = self.session.query(User).filter_by(email = self.__inputs.email).first()
+
+        if query is None !=True:
+            return {
+            "status": "failed",
+            "message": "User already exists!"
+            }
+
         self.session.add(sql)
         self.session.commit()
 
@@ -27,3 +51,4 @@ class Registration:
             "status": "passed",
             "message": "You have successfully registered!"
         }
+
